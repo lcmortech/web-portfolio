1fef8544f0f66d62aa5397d0e0bccdb257cafd60
Lauren <rg326@live.com>
2022-04-22 20:21:31 -0400

    worked on outline for projects sidebar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,14 @@
 
 
 function Projects() {
+    <aside>
+        <h4>RetroLand - EStore</h4>
+        <p>Coming Soon!</p>
+        <h4>Triple Threat - An Original Card Game</h4>
+        <p>Coming Soon!</p>
+        <h4>Toon Tunnel</h4>
+        <p>Coming Soon!</p>
+    </aside>
 }
 
 export default Projects;
