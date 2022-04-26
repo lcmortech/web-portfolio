2f343867bd613ba3bc1b5c5a5621e58803667ffa
Lauren <rg326@live.com>
2022-04-26 17:21:52 -0400

    replaced lorem ipsum blurb with text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,9 +5,7 @@ function AboutMe() {
     <aside>
         <img src="../public/cartoon-ren.png" alt="picture of me" />
         <h1>Hi! I'm Lauren!</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia numquam unde corporis animi? Earum, modi magni excepturi alias, aspernatur minima facere laudantium ex dolorum illum assumenda sequi dolores voluptatibus quasi.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga natus minus laudantium omnis ad, molestiae temporibus? Amet optio quia, illo accusamus animi, voluptatibus ab rerum inventore laudantium tenetur tempora saepe!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi facere fugiat voluptatibus voluptas ad nisi quidem, magni earum, quos voluptate incidunt cum temporibus provident aliquam quia sequi nobis harum adipisci!
+        <p> I'm a Javascript developer from the New York City area. I started coding back in 2019, and my main focus is web development and building applications for various language learning solutions! Thanks for stopping by!
         </p>
     </aside>
     
