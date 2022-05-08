589434ff87300477c221dc757a38dee56ea2d6bc
Lauren Morgan <rg326@live.com>
2022-05-07 20:50:52 -0400

    added border radius to image, some basic styling to AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,14 +1,14 @@
-
+import myImage from '../cartoon-ren.png'
+import '../App.css'
 
 function AboutMe() {
  return(  
-    <aside>
-        <img src="../public/cartoon-ren.png" alt="picture of me" />
+    <aside className="side-profile">
+        <img className="image-border" src={myImage} alt="cartoon me" width="150" />
         <h1>Hi! I'm Lauren!</h1>
         <p> I'm a Javascript developer from the New York City area. I started coding back in 2019, and my main focus is web development and building applications for various language learning solutions! I mainly work in front-end, but occasionally set up servers as well! Thanks for stopping by!
         </p>
     </aside>
-    
  )
 }
 
