b95a4589afcfdef771a48a6194f16b4ee9f17c5f
Lauren Morgan <rg326@live.com>
2022-05-09 18:39:01 -0400

    changed layout style, added animation to header, added spacing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Services from './components/Services';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
 import Navigation from './components/Navigation';
+import cubeImage from './cube-image.jpeg'
 
 function App() {
   return (
     <div className="App">
+      <img className='cover-image' src={cubeImage} />
       <HomePage />
       <Services />
       <Resume />
--- src/components/HPHeader.js
+++ src/components/HPHeader.js
@@ -4,8 +4,8 @@ function HPHeader() {
     return( 
         <header>
             <section class="header-container">
-                <h1 className="animated-header">Lauren Morgan</h1>
-                <p className="animated-header">Javascript Developer</p>
+                <h1 className="animated-header-1">Lauren Morgan</h1>
+                <p className="animated-header-2">Javascript Developer</p>
             </section>
         </header>
     )
