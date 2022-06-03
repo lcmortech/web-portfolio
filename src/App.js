7899695d7bde3f99a2e0f6b8d9afa63f629eb6f3
Lauren Morgan <rg326@live.com>
2022-06-02 21:05:47 -0400

    added routes to App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
 import Services from './components/Services';
@@ -6,16 +7,29 @@ import Contact from './components/Contact';
 import Navigation from './components/Navigation';
 import cubeImage from './cube-image.jpeg'
 
-function App() {
-  return (
-    <div className="App">
-      {/*<img className='cover-image' src={cubeImage} />*/}
-      <Navigation />
-      <HomePage />
-      <Services />
-      <Resume />
-    </div>
-  );
+
+function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/*<img className='cover-image' src={cubeImage} />*/}
+//       <Navigation />
+//       <HomePage />
+//       <Services />
+//       <Resume />
+//     </div>
+//   );
+// }
 
 export default App;
